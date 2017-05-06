0bf6f41f66bb8290de64c71cd39231923c56507d
esauOp <monkeycoss@gmail.com>
2017-05-06 09:40:12 -0500

    JSON format

--- web/views/api/v1/movie_view.ex
+++ web/views/api/v1/movie_view.ex
@@ -2,15 +2,15 @@ defmodule Movie.Api.V1.MovieView do
     use Movies.Web, :view
 
   def render("index.json", %{movies: movies}) do
-    %{data: render_many(movies, Movie.Api.V1.MovieView, "movie.json")}
+    render_many(movies, Movie.Api.V1.MovieView, "movie.json")
   end
 
   def render("show.json", %{movie: movie}) do
-    %{data: render_one(movie, Movie.Api.V1.MovieView, "movie.json")}
+    render_one(movie, Movie.Api.V1.MovieView, "movie.json")
   end
 
   def render("movie.json", %{movie: movie}) do
-    %{id: movie.id,
+    %{
     title: movie.title,
     summary: movie.summary,
     year: movie.year}
